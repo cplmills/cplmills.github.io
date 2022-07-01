90fff809d6103e17341152599c4834878db9ad6a
cplmills <91394219+cplmills@users.noreply.github.com>
2022-07-01 19:59:41 +1000

    prophets update

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -1,6 +1,7 @@
-function prophets(){
+// function prophets(){
     console.log('hello');
-    const requestURL = './latter-day-prophets.json';
+    const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+    
     fetch(requestURL)
         .then(function (response) {
             return response.json();
@@ -8,13 +9,25 @@ function prophets(){
         .then(function (jsonObject) {
             console.table(jsonObject);  // temporary checking for valid response and data parsing
             const prophets = jsonObject['prophets'];
+            console.log(prophets)
             for (let i = 0; i < prophets.length; i++ ) {
                 let card = document.createElement('section');
                 let h2 = document.createElement('h2');
+                let p1 = document.createElement('p');
+                let p2 = document.createElement('p');
+                let img = document.createElement('img');
+
                 h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+                p1.innerHTML = 'Date of Birth: ' + prophets[i].birthdate;
+                p2.innerHTML = 'Place of Birth: ' + prophets[i].birthplace;
+                img.setAttribute('src', prophets[i].imageurl);
+
                 card.appendChild(h2);
+                card.appendChild(p1);
+                card.appendChild(p2);
+                card.appendChild(img);
                 document.querySelector('div.cards').appendChild(card);
             }
         }
         );
-}
+// }
