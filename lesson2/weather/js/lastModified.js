b34a736170eef11d6dc49ee0271e3df559e07f9d
cplmills <91394219+cplmills@users.noreply.github.com>
2022-07-01 18:13:01 +1000

    relink again!

--- lesson2/weather/js/lastModified.js
+++ lesson2/weather/js/lastModified.js
@@ -2,5 +2,4 @@ function lastModified() {
     let date = new Date(document.lastModified);  
     dateString = date.toLocaleString('default', {weekday: 'long'}) + ', ' + date.getDate() + ' ' + date.toLocaleString('default', {month: 'long'}) + ' ' + date.getFullYear();
     document.querySelector("#lastModified").innerHTML = "Last Updated: " + dateString;
-
 }
